45d30397339eae6f342483ea0a42dace20e1751e
Chetansinh-r <157673062+Chetansinh-r@users.noreply.github.com>
2025-07-18 13:00:53 +0530

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "candlestickchart",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "candlestickchart",
+            targets: ["candlestickchart"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "candlestickchart",
+            url: "https://github.com/Chetansinh-r/ChartLib/releases/download/v1.0.0/candlestickchart.zip",
+            checksum: "46a67ddc65e1c2ee07c632d989dc6898e69f58b28ca1b32fd98bdaf89b1a5faa"
+        ),
+    ]
+)
